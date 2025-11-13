1d34d287131a88424c4c8716c806fbd5228ec2e1
julineamiot <julineamiot03@gmail.com>
2025-11-13 12:17:52 +0100

    écriture fonction

--- Projet1.py
+++ Projet1.py
@@ -1,1 +1,21 @@
 print("Bonjour")
+
+
+class ReseauNeurones:
+    def __init__(self):
+        pass
+
+    def ouvrirImage(self):
+        pass
+
+    def forwardPropag(self):
+        pass
+
+    def backPropag(self):
+        pass
+
+
+def main():
+    pass
+
+
