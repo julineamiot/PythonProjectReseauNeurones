ead947a31ac0cbd52e80e0252385a3b1738205fe
Utilisateur <henridesmottes@gmail.com>
2025-11-19 20:21:19 +0100

    def ouvrirImage et forwardPropag

--- Projet1.py
+++ Projet1.py
@@ -3,19 +3,24 @@ print("Bonjour")
 
 class ReseauNeurones:
     def __init__(self):
+        #on initalise tous les paramètres du réseau de neurones (les poids  w, les biais b)
+        self.w = None
+        self.biais = None
+
+    def ouvrirImage(self, chemin):
+        #Cette fonction ouvre une image et la convertit en matrice numpy,  avec des niveaux de gris entre 0 et 255.
         pass
 
-    def ouvrirImage(self):
-        pass
-
-    def forwardPropag(self):
+    def forwardPropag(self, image_matrice):
+        #cette fonction calcule la sortie du réseau, ie donne 0 ou 1. Elle  trasnforme la matrice de pixels en une prédiction.
+        #elle combine les pixels avec les poids pour donner un score, et  renvoie 0 ou 1 selon le score "
         pass
 
     def backPropag(self):
-        pass teedt
+            pass teedt
 
 
-def main():
-    pass
+    def main():
+        pass
 
 
