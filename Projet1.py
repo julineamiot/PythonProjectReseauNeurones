a25ba3511251288fc15ce60b31c7550d7e0a4c38
julineamiot <julineamiot03@gmail.com>
2025-11-26 20:46:22 +0100

    forward propag

--- Projet1.py
+++ Projet1.py
@@ -25,16 +25,18 @@ class ReseauNeurones:
         self.poidsSortie = np.random.random_sample(self.nbNeuronesCouche[1], 1)
 
     def forwardPropag(self, imageMatrice):
-        #cette fonction calcule la sortie du réseau, ie donne 0 ou 1. Elle  trasnforme la matrice de pixels en une prédiction.
-        #elle combine les pixels avec les poids pour donner un score, et  renvoie 0 ou 1 selon le score "
-        pix = [imageMatrice[i][j] for i in range(imageMatrice.shape[0]) for j in range(imageMatrice.shape[1])]
-        taillepix = len(pix)
-        matPoids = np.random.random_sample((taillepix, self.nbNeuronesCouche))
-        
-        for i in pix:
-            i = i * 
+        # on transforme la matrice de pixels en vecteur
+        pix = imageMatrice.reshape(-1)
 
-        pass
+        # produit matriciel entrée x couche cachée
+        z1 = np.dot(pix, self.poidsEntree) #np.dot pour les produits np
+        a1 = self.fonctionActivation(z1)
+
+        # produit matriciel cachée x sortie
+        z2 = np.dot(a1, self.poidsSortie)
+        a2 = self.fonctionActivation(z2)
+
+        return a2
 
     def backPropag(self, image_matrice, classif):
         "cette fonction met à jour les poids du réseau en fonction de l'erreur. Elle prend en entrée l'image et la classification"
@@ -45,9 +47,5 @@ class ReseauNeurones:
         "elle gère l'entraînement et les tests du réseau de neurones. Elle appelle forward et backward pour plusieurs images différentes "
         pass
 
-"""mat = np.arange(6).reshape(2, 3)
-print(mat)
-pix = [mat[i][j] for i in range(mat.shape[0]) for j in range(mat.shape[1])]
-print(pix)"""
 
 """le porgramme doit orendre en entrée une matrcie numpy de taille paramétrable. Le réseau doit avir une couche d'entrée de la tille de l'image et à la fin on a une sortie. Pour chaque couche, faire produit matriciel entre les indices de la matrice avec poids. Si valeur du neurone inféireur à 0, on renvoie -1, sinon on renvoie 1"""
