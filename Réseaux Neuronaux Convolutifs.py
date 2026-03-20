7b56328156f59a52828ad75a7597ca15a147f765
Utilisateur <henridesmottes@gmail.com>
2026-03-20 10:40:26 +0100

    programme

--- Réseaux Neuronaux Convolutifs.py
+++ Réseaux Neuronaux Convolutifs.py
@@ -2,16 +2,22 @@
 on va donc obtenir 4 images différentes selon le filtre appliqué
 normaliser l'image
 multiplier la valeur du pixel par la valeur du filtre
-centrer le filtre et faire addition des multiplications et obtenir un seul chiffre
+centrer le filtre et faire addition des multiplications avec biais et obtenir un seul chiffre
 faire du pading à 0
 le but est d'obtenir des nouveaux paterne sur l'image
 stride :  de combien on décale la matrice de filtre
 le réseau de neuronne va apprendre les chiffres qui sont dans le filtre
 permet de compresser le nb dinformations à connaitre
-pooling : réduire l'information et résumer l'information (moyenne, + grande valeur)
+après la convolution il y a une fonction d'activation de type Relu (appliqué sur chaque éléments de sortie de la convolution)
+pooling : réduire l'information et résumer l'information (Average Pooling = moyenne, + Max Pooling = grande valeur (c'est le + utilisé))
+Flatten : on doit mettre en ligne toutes les infos pour que la phase dense puisse les utiliser
 dense : mettre toutes les info en un veteur (si beaucoup d'image on peut faire 2 fois cette étape) le nb de dense correspond au nb de neurones que l'on doit avoir en sortie
-pour éviter le sur apprentissage, on peut tourner, flouter l'image
-carte de saillance : dire que l'on a beaucoup utiliser pour détecter l'objet"""
+pour éviter le sur apprentissage (= Overfitting), on peut tourner, flouter l'image
+carte de saillance : dire que l'on a beaucoup utiliser pour détecter l'objet (repose sur le calcul du gradient)
+
+1ere couche 64 neurones puis 128 pour les suivantes
+
+(convolution => activation (Relu) => pooling) * (x fois) => dense => prediction"""
 
 
 import struct
